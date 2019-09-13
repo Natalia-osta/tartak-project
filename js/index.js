5a31b60927a3689fcd6445b42f0c11f171463f74
Natalia-osta <ostas.natali@gmail.com>
2019-09-13 07:45:13 +0000

    update slider

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function toogleMenu(evt){
 }
 
 
-  const sliderItem = document.querySelector('.slider-item1');
+  /*const sliderItem = document.querySelector('.slider-item1');
   const sliderItemToo = document.querySelector('.slider-item2');
   const sliderItemtree = document.querySelector('.slider-item3');
 
@@ -23,4 +23,21 @@ function toogleMenu(evt){
       sliderItemtree.addEventListener('click', numPressedTree);
     function numPressedTree(ev) {
         document.querySelector(".header").style.background ="url('../img/slider/3.jpg')";
+    }*/
+
+    /*const header = document.querySelector(".header");*/
+const items = document.querySelectorAll(".slider-nav  li.slider-item");
+
+items.forEach( li => li.addEventListener("click", itemPressed));
+
+function itemPressed(ev){
+    ev.preventDefault();
+    if (items[0] === ev.target){
+        document.querySelector("header").style.background ="url('../img/slider/1.jpg')";
     }
+    else if (items[1] === ev.target){
+        document.querySelector("header").style.background ="url('../img/slider/2.jpg')";
+}
+else
+document.querySelector("header").style.background ="url('../img/slider/3.jpg')";
+}
