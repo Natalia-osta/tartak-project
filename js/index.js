028c73aa3fad0e9f16a2c964e729c3a7f4732c1f
Natalia-osta <ostas.natali@gmail.com>
2019-09-13 13:17:11 +0000

    update slider

--- js/index.js
+++ js/index.js
@@ -8,36 +8,25 @@ function toogleMenu(evt){
 }
 
 
-  /*const sliderItem = document.querySelector('.slider-item1');
-  const sliderItemToo = document.querySelector('.slider-item2');
-  const sliderItemtree = document.querySelector('.slider-item3');
 
-    sliderItem.addEventListener('click', numPressedFirst);
-    function numPressedFirst(ev) {
-        document.querySelector(".header").style.background ="url('../img/slider/1.jpg')";
-    }
-      sliderItemToo.addEventListener('click', numPressedSecond);
-    function numPressedSecond(ev) {
-        document.querySelector(".header").style.background ="url('../img/slider/2.jpg')";
-    }
-      sliderItemtree.addEventListener('click', numPressedTree);
-    function numPressedTree(ev) {
-        document.querySelector(".header").style.background ="url('../img/slider/3.jpg')";
-    }*/
-
-    /*const header = document.querySelector(".header");*/
+const header = document.querySelector("header");
 const items = document.querySelectorAll(".slider-nav  li.slider-item");
 
 items.forEach( li => li.addEventListener("click", itemPressed));
 
 function itemPressed(ev){
-    ev.preventDefault();
     if (items[0] === ev.target){
-        document.querySelector("header").style.background ="url('../img/slider/1.jpg')";
-    }
-    else if (items[1] === ev.target){
-        document.querySelector("header").style.background ="url('../img/slider/2.jpg')";
+        header.style.background ="url('../img/slider/1.jpg')";
+        header.style.transition=".5s linear";
 }
-else
-document.querySelector("header").style.background ="url('../img/slider/3.jpg')";
+    else if (items[1] === ev.target){
+        header.style.background ="url('../img/slider/2.jpg')";
+        header.style.backgroundSize ="cover";
+        header.style.transition=".5s linear";
+        this.classList.toggle('item-bg-off');
+}
+    else
+        header.style.background ="url('../img/slider/3.jpg')";
+        header.style.backgroundSize ="cover";
+        header.style.transition=".5s linear";
 }
